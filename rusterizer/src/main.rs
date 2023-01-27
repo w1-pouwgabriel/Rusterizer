ae539eb773fa4996b2953f46a0248c2678b2323b
Gabriël Pouw <gabriel@pouw.com>
2023-01-27 13:53:35 +0100

    Using berycentric coordinates to linearly lerp between colors

--- rusterizer/src/main.rs
+++ rusterizer/src/main.rs
@@ -1,54 +1,31 @@
 extern crate minifb;
-use glam::Vec2;
+
+use glam::{Vec2, Vec3};
 
 use triangle::Triangle;
 use window::Window;
-
+use utils::*;
 
 mod triangle;
 mod window;
+mod utils;
 
 const WIDTH: usize = 640;
 const HEIGHT: usize = 360;
 
-pub fn to_argb32(a: u8, r: u8, g: u8, b: u8) -> u32{
-    let mut color:u32 = a as u32;
-    color = (color << 8) + r as u32;
-    color = (color << 8) + g as u32;
-    color = (color << 8) + b as u32;
-    
-    color
-}
-
-//clockwise
-pub fn edge_function(v0: Vec2, v1: Vec2, p: Vec2) -> f32 {
-    (p.x - v0.x) * (v1.y - v0.y) - (p.y - v0.y) * (v1.x - v0.x)
-}
-
-//num items in a row
-pub fn index_to_coords(p: usize, width: usize) -> (usize, usize) {
-    (p % width, p / width)
-}
-
-pub fn scan_line() -> u32{
-    50u32
-}
-
-pub fn aabb_check(min: Vec2, max: Vec2, point: Vec2) -> bool
-{
-    if point.x > min.x && point.y > min.y{
-        if point.y < max.y && point.y < max.y{
-            return true;
-        }
-    }
-
-    false
+pub fn barycentric_coordinates(point: Vec2, v0: Vec2, v1: Vec2, v2: Vec2, area: f32) -> Vec3 {
+    let m0 = edge_function(point, v1, v2);
+    let m1 = edge_function(point, v2, v0);
+    let m2 = edge_function(point, v0, v1);
+    // instead of 3 divisions we can do 1/area *
+    let a = 1.0 / area;
+    glam::vec3(m0 * a, m1 * a, m2 * a)
 }
 
 fn main() {
     let mut window = Window::new("Rusterizer - ESC to exit".to_string(), WIDTH, HEIGHT);
 
-    let triangle = Triangle::new(glam::vec2(270.0, 150.0), glam::vec2(370.0, 200.0), glam::vec2(480.0, 150.0));
+    let triangle = Triangle::new(glam::vec2(150.0, 150.0), glam::vec2(370.0, 300.0), glam::vec2(480.0, 150.0));
 
     // Limit to max ~60 fps update rate
     window.limit_fps(Some(60));
@@ -60,37 +37,19 @@ fn main() {
             // shadowing a variable
             let coords = glam::vec2(coords.0 as f32, coords.1 as f32);
 
-            if aabb_check(triangle.min, triangle.max, coords) {
-                let m2 = edge_function(coords, triangle.v0, triangle.v1);
+            // Triangle area
+            let area = edge_function(triangle.v0, triangle.v1, triangle.v2);
+            // subtriangles area / triangle area
+            let bary = barycentric_coordinates(coords, triangle.v0, triangle.v1, triangle.v2, area);
 
-                let m0 = edge_function(coords, triangle.v1, triangle.v2);
-                let m1 = edge_function(coords, triangle.v2, triangle.v0);
-                
-                // if m0 & m1 & m2 >= 0 we are inside the triangle
-                if m0 >= 0.0 && m1 >= 0.0 && m2 >= 0.0 {
-                    window.frame_buffer().set_pixel(coords.x as usize, coords.y as usize, to_argb32(
-                        255,
-                        255.0 as u8,
-                        80.0 as u8,
-                        80.0 as u8,
-                    ));
-                }else{
-                    window.frame_buffer().set_pixel(coords.x as usize, coords.y as usize, to_argb32(
-                        255,
-                        0.0 as u8,
-                        0.0 as u8,
-                        0.0 as u8,
-                    ));
-                }
-            } 
-            else{
-                window.frame_buffer().set_pixel(coords.x as usize, coords.y as usize, to_argb32(
-                    255,
-                    0.0 as u8,
-                    0.0 as u8,
-                    0.0 as u8,
-                ));
-            }
+            window.frame_buffer().set_pixel(coords.x as usize, coords.y as usize, to_argb32(
+                255,
+                (bary.x * 255.0) as u8,
+                (bary.y * 255.0) as u8,
+                (bary.z * 255.0) as u8,
+            ));
+
+            
         }
 
         // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
---
+++ rusterizer/src/utils.rs
@@ -0,0 +1,31 @@
+use glam::Vec2;
+
+pub fn to_argb32(a: u8, r: u8, g: u8, b: u8) -> u32{
+    let mut color:u32 = a as u32;
+    color = (color << 8) + r as u32;
+    color = (color << 8) + g as u32;
+    color = (color << 8) + b as u32;
+    
+    color
+}
+
+//clockwise
+pub fn edge_function(v0: Vec2, v1: Vec2, p: Vec2) -> f32 {
+    (p.x - v0.x) * (v1.y - v0.y) - (p.y - v0.y) * (v1.x - v0.x)
+}
+
+//num items in a row
+pub fn index_to_coords(p: usize, width: usize) -> (usize, usize) {
+    (p % width, p / width)
+}
+
+pub fn aabb_check(min: Vec2, max: Vec2, point: Vec2) -> bool
+{
+    if point.x > min.x && point.y > min.y{
+        if point.y < max.y && point.y < max.y{
+            return true;
+        }
+    }
+
+    false
+}
